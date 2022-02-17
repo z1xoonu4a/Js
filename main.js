dbb5f05e09371f103a90bc43c4ef8e4fe62db837
Z1xonu4a <hedhet68@gmail.com>
2022-02-17 08:42:33 +0300

    "master"

--- main.js
+++ main.js
@@ -1,26 +1,5 @@
-const inputTodo = document.querySelector('#todo-input')
-const countTodo = document.querySelector('#countTodo')
-const todoFrom = document.querySelector('#todo-form')
-const listTodo = document.querySelector('#todo-list')
-const button = document.querySelector('button')
+const button = document.querySelector('#start')
 
-todoFrom.addEventListener('submit', fromHandler)
-
-let count = 0
-
-function fromHandler(event) {
-	event.preventDefault()
-
-	const valueInput = inputTodo.value
-	const li = document.createElement('li')
-	const ul = document.querySelector('ul')
-
-	if (valueInput.length >= 1) {
-		count++
-		li.innerText = valueInput
-		ul.append(li)
-	}
-
-	countTodo.innerHTML = `Todo: ${count}`
-	inputTodo.value = ''
-}
+button.addEventListener('click', () => {
+	console.log(1)
+})
