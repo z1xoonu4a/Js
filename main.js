e2d022a0a443e153a51f46cb217f81f57c3233ae
Z1xonu4a <hedhet68@gmail.com>
2022-02-23 01:54:34 +0300

    "master"

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ async function getCourse() {
 	const response = await fetch('https://www.cbr-xml-daily.ru/daily_json.js')
 	const dataJson = await response.json()
 
-	idUsd.innerHTML = `${dataJson.Valute.EUR.Value.toFixed(0)} рублей за доллар.`
-	idEuro.innerHTML = `${dataJson.Valute.USD.Value.toFixed(0)} рублей за евро.`
+	idUsd.innerHTML = `${dataJson.Valute.USD.Value.toFixed(0)} рублей за доллар.`
+	idEuro.innerHTML = `${dataJson.Valute.EUR.Value.toFixed(0)} рублей за евро.`
 }
 
 getCourse()
