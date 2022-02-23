f77bae363719b5138908eb2b62e278c6edda3e7b
Z1xonu4a <hedhet68@gmail.com>
2022-02-23 22:24:35 +0300

    "master"

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-const usd = document.querySelector('#usd')
-const eur = document.querySelector('#eur')
-
-async function getCourse() {
-	const response = await fetch('https://www.cbr-xml-daily.ru/daily_json.js')
-	const dataJson = await response.json()
-
-	usd.innerHTML = `${dataJson.Valute.USD.Value.toFixed(0)} рублей за доллар.`
-	eur.innerHTML = `${dataJson.Valute.EUR.Value.toFixed(0)} рублей за евро.`
-}
-
-getCourse()
-
 // Timer
 const timer = document.querySelector('#timer')
 const btnStart = document.querySelector('#startTimer')
