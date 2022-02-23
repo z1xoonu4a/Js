2589935474fbce0c684924f5b38ba4a307ba3e17
Z1xonu4a <hedhet68@gmail.com>
2022-02-23 21:15:00 +0300

    "master"

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Course
 const idUsd = document.querySelector('#usd')
 const idEuro = document.querySelector('#eur')
 
@@ -10,3 +11,53 @@ async function getCourse() {
 }
 
 getCourse()
+
+// Timer
+const timer = document.querySelector('#timer')
+const btnStart = document.querySelector('#startTimer')
+const btnStop = document.querySelector('#stopTimer')
+const btnReset = document.querySelector('#resetTimer')
+
+let timerStarted = false
+let intervalId
+let second = 0
+let minute = 0
+let hour = 0
+
+btnStart.addEventListener('click', timerStart)
+btnStop.addEventListener('click', timerStop)
+btnReset.addEventListener('click', timerReset)
+
+function timerStart() {
+	if (!timerStarted) {
+		timerStarted = true
+		intervalId = setInterval(() => {
+			if (second === 60) {
+				minute++
+				second = 0
+				timer.innerHTML = `${hour}:${minute}:${second}`
+			} else if (minute === 60) {
+				hour++
+				minute = 0
+				timer.innerHTML = `${hour}:${minute}:${second}`
+			} else {
+				second++
+				timer.innerHTML = `${hour}:${minute}:${second}`
+			}
+		}, 1000)
+	}
+}
+
+function timerStop() {
+	clearInterval(intervalId)
+	timerStarted = false
+}
+
+function timerReset() {
+	clearInterval(intervalId)
+	second = 0
+	minute = 0
+	hour = 0
+	timer.innerHTML = `${hour}:${minute}:${second}`
+	timerStarted = false
+}
